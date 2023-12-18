893e95949972cbd69ee04f0f77061fff2c2ee074
juanfranflores <juan27fp@gmail.com>
2023-12-17 22:48:04 -0300

    Refactor error handling and *.json

--- json/send_json.py
+++ json/send_json.py
@@ -1,9 +1,14 @@
 """
-This module provides functionality for sending JSON data using MQTT.
+This script sends JSON data via MQTT using the paho-mqtt client. 
+It connects to the broker at "192.168.1.11" on port 1883.
+
+Functions:
+- on_connect(client, userdata, flags, rc): Connects to the MQTT broker
 """
-import json
+import sys
 import time
-import sys  # import the sys module
+import json
+import glob
 import paho.mqtt.client as mqtt
 
 
@@ -20,28 +25,60 @@ def on_connect(client, userdata, flags, rc):
     Returns:
     None
     """
-    print("Connected with result code "+str(rc))
+    if rc == 0:
+        print("Connected successfully.")
+    else:
+        print(f"Connection failed with code {rc}.")
+        sys.exit()
 
 
 client = mqtt.Client()
 client.on_connect = on_connect
 
-client.connect("192.168.1.11", 1883, 60)
+# Connect to the MQTT broker and error handling
+try:
+    client.connect("192.168.1.11", 1883, 60)
 
-# Check if a filename was passed as a command-line argument
-if len(sys.argv) != 2:
-    print("Usage: python send_json.py <filename>")
+except ConnectionRefusedError:
+    print("Connection refused. Please check your broker settings.")
+    sys.exit(1)
+except ConnectionError:
+    print("Connection failed. Please check your network connection.")
+    sys.exit(1)
+except TimeoutError:
+    print("Connection timed out. Please check your broker settings or network connection.")
+    sys.exit(1)
+except OSError as e:
+    print(f"OS error occurred: {e}")
+    sys.exit(1)
+except Exception as e:
+    print(f"Unexpected error occurred: {e}")
     sys.exit(1)
 
-# Load the JSON data from the file passed as a command-line argument
-with open(sys.argv[1], encoding='utf-8') as f:
-    data = json.load(f)
+# Start the MQTT loop
+client.loop_start()
 
-# Extract the topic and payload from the data
-topic = data['topic']
-payload = data['payload']
+# Check if a filename was passed as a command-line argument
+if len(sys.argv) > 1:
+    # A filename was provided, send only that file
+    filenames = sys.argv[1:]
+else:
+    # No filename was provided, send all .json files in the current directory
+    filenames = glob.glob('*.json')
 
-client.loop_start()  # start the loop
-client.publish(topic, json.dumps(payload), qos=1)
-time.sleep(1)  # add a small delay to ensure the message gets sent
+# Loop over the list of filenames
+for filename in filenames:
+    # Load the JSON data from the file
+    with open(filename, encoding='utf-8') as f:
+        data = json.load(f)
+
+    # Extract the topic and payload from the data
+    topic = data['topic']
+    payload = data['payload']
+
+    # Publish the payload to the topic
+    client.publish(topic, json.dumps(payload), qos=1)
+    print(f"Published {payload} to {topic}")
+    time.sleep(1)  # add a small delay to ensure the message gets sent
+
 client.loop_stop()  # stop the loop
